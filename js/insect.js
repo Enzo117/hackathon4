9943717e29d81dafdc2058c1a8762c874c3a749c
Enzo117 <58591382+Enzo117@users.noreply.github.com>
2019-12-16 17:52:29 +0100

    Social Media buttons und Texte bearbeitet

--- js/insect.js
+++ js/insect.js
@@ -7,4 +7,20 @@ $('a[href^="#"]').on('click',function(e) {
  }, 900, 'swing', function () {
   window.location.hash = target;
  });
+
+ $('.fa').hover(
+   function(){
+     $(this).stop().animate({
+     width: '275px',
+     height: '275px',
+     top:'12.5px'},
+     200);},
+
+     function(){
+       $(this).stop().animate({
+       width: '250px',
+       height: '250px',
+       top:'0'},
+       200);}
+       );
 });
